7e80c3c8244fd0c70b68a9aeea1fcaf9fd6e8fcc
SoraSuegami <suegamisora@gmail.com>
2023-03-16 17:49:27 +0700

    Support multi compression chips.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod sha256;
-
 pub use eth_types::Field;
 pub use sha256::*;
-pub use zkevm_circuits::sha256_circuit::sha256_bit::{Sha256BitChip, Sha256BitConfig};
+pub use zkevm_circuits::sha256_circuit::{
+    sha256_compression::{Sha256AssignedRows, Sha256CompressionConfig},
+    util::H,
+};
